19d2002c7b29d997cac5bc279f8103752a863951
Vishal Khurana <98726988+iamvishalkhurana@users.noreply.github.com>
2023-06-12 19:11:25 +0530

    Added solution - LeetHub

--- Postorder Traversal - GFG/postorder-traversal.cpp
+++ Postorder Traversal - GFG/postorder-traversal.cpp
@@ -124,16 +124,42 @@ struct Node
 }; */
 
 
-void postorder(Node* root,vector<int>&v){
-        if(root==NULL) return;
-        
-        postorder(root->left,v);
-        postorder(root->right,v);
-        v.push_back(root->data);
-    }
 vector <int> postOrder(Node* root)
 {
-  vector<int>post;
-  postorder(root,post);
+  vector<int>post,in,pre;
+  stack<pair<Node*,int>>st;
+  st.push({root,1});
+  
+  while(!st.empty()){
+      
+      auto temp=st.top();
+      st.pop();
+      
+      if(temp.second==1){
+          pre.push_back(temp.first->data);
+          temp.second++;
+          st.push(temp);
+          if(temp.first->left){
+              st.push({temp.first->left,1});
+          }
+      }
+      
+      else if(temp.second==2){
+          in.push_back(temp.first->data);
+          temp.second++;
+          st.push(temp);
+          if(temp.first->right){
+              st.push({temp.first->right,1});
+          }
+          
+      }
+      else{
+          post.push_back(temp.first->data);
+          
+      }
+  }
   return post;
+  
+  
+  
 }
