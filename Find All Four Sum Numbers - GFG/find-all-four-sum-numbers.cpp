13eb444030ef0f0463b7067e88e3dc3105e070d2
Vishal Khurana <98726988+iamvishalkhurana@users.noreply.github.com>
2023-06-29 16:15:30 +0530

    Added solution - LeetHub

--- Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
@@ -13,10 +13,14 @@ class Solution{
     vector<vector<int> > fourSum(vector<int> &arr, int target) {
         int n=arr.size();
         sort(arr.begin(),arr.end());
-        set<vector<int>>s;
+        vector<vector<int>>ans;
         
         for(int i=0;i<n;i++){
+            if(i>0 && arr[i]==arr[i-1]) continue;
+            
             for(int j=i+1;j<n;j++){
+                if(j>i+1 && arr[j]==arr[j-1]) continue;
+                
                 int k=j+1;
                 int l=n-1;
                 while(k<l){
@@ -27,15 +31,18 @@ class Solution{
                     
                     if(sum==target){
                         vector<int>x={ arr[i],arr[j],arr[k],arr[l]};
-                        s.insert(x);
+                        ans.push_back(x);
                         k++;l--;
+                        
+                        while(k<l && arr[k]==arr[k-1]) k++;
+                        while(k<l && arr[l]==arr[l+1]) l--;
+                        
                     }
                     else if(sum< target) k++;
                     else l--;
                 }
             }
         }
-        vector<vector<int>>ans(s.begin(),s.end());
         return ans;
     }
 };
