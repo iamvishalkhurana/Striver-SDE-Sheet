f6964a9a237fba070450528299c5f8aea08215b2
Vishal Khurana <98726988+iamvishalkhurana@users.noreply.github.com>
2023-07-01 12:46:15 +0530

    Added solution - LeetHub

---
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    void dfs(vector<vector<int>>& image,vector<vector<int>>&ans, int sr, int sc, int newColor,int prev){
+        ans[sr][sc]=newColor;
+        
+        int n=image.size(),m=image[0].size();
+        
+        if(sr-1>=0 && image[sr-1][sc]==prev && ans[sr-1][sc]!=newColor){
+            dfs(image,ans,sr-1,sc,newColor,prev);
+        }
+        if(sc-1>=0 && image[sr][sc-1]==prev && ans[sr][sc-1]!=newColor){
+            dfs(image,ans,sr,sc-1,newColor,prev);
+        }
+        if(sc<m-1 && image[sr][sc+1]==prev && ans[sr][sc+1]!=newColor){
+            dfs(image,ans,sr,sc+1,newColor,prev);
+        }
+        if(sr<n-1 && image[sr+1][sc]==prev && ans[sr+1][sc]!=newColor){
+            dfs(image,ans,sr+1,sc,newColor,prev);
+        }
+        
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        
+        int n=image.size(),m=image[0].size();
+        
+        int prev=image[sr][sc];
+        
+        vector<vector<int>>ans=image;
+        dfs(image,ans,sr,sc,newColor,prev);
+        
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>image(n, vector<int>(m,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++)
+				cin >> image[i][j];
+		}
+		int sr, sc, newColor;
+		cin >> sr >> sc >> newColor;
+		Solution obj;
+		vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
+		for(auto i: ans){
+			for(auto j: i)
+				cout << j << " ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
