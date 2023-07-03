9f020e5d9c10760bb7eea93d4d57f502d18d0d69
Vishal Khurana <98726988+iamvishalkhurana@users.noreply.github.com>
2023-07-03 12:38:23 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -27,20 +27,44 @@ class Solution {
         }
         
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
-            vector<int>visited(V,0);
-            for(int i=0;i<V;i++){
-                if(!visited[i]){
-                    detect(adj,i,visited);
-                }
-            }
             
-            vector<int>x;
-            for(int i=0;i<V;i++){
-                if(visited[i]==2){
-                    x.push_back(i);
+        vector<int>newgraph[V];
+
+        for(int i=0;i<V;i++){
+            for(int j:adj[i]){
+                newgraph[j].push_back(i);
+            }
+        }
+        
+        vector<int>indegree(V,0);
+        
+        for(int i=0;i<V;i++){
+            for(int j:newgraph[i]) indegree[j]++;
+        }
+        
+        queue<int>q;
+        
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        vector<int>ans;
+        
+        while(!q.empty()){
+            int t=q.front();q.pop();
+            ans.push_back(t);
+            
+            for(int x:newgraph[t]){
+                indegree[x]--;
+                if(indegree[x]==0){
+                    q.push(x);
                 }
             }
-            return x;
+        }
+        
+        sort(ans.begin(),ans.end());
+        return ans;
+        
     }
 };
 
