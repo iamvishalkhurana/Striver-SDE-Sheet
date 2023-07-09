6f75d32848a064e0ea9b30a2f97f8b418bb09d1b
Vishal Khurana <98726988+iamvishalkhurana@users.noreply.github.com>
2023-07-09 20:29:45 +0530

    Added solution - LeetHub

---
+++ Maximum Stone Removal - GFG/maximum-stone-removal.cpp
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class DisjointSet {
+public: 
+    vector<int> rank, parent, size;
+    DisjointSet(int n){
+            rank.resize(n+1,0);
+            size.resize(n+1,1);
+            parent.resize(n+1);
+            
+            for(int i=0;i<=n;i++) parent[i]=i;
+            
+        }
+        
+        int findU_parent(int n){
+            if(parent[n]==n){
+                return n;
+            }
+            return parent[n]=findU_parent(parent[n]);
+        }
+        
+        void unionByRank(int i,int j){
+            int ult_i=findU_parent(i);
+            int ult_j=findU_parent(j);
+            
+            if(ult_i == ult_j) return; 
+            
+            if(rank[ult_i] < rank[ult_j]){
+                parent[ult_i]=ult_j;
+            }
+            else if(rank[ult_i]>rank[ult_j]){
+                parent[ult_j]=ult_i;
+            }
+            
+            else{
+                parent[ult_j]=ult_i;
+                rank[ult_i]++;
+            }
+            
+        }
+        
+        void unionBySize(int i,int j){
+            int ult_i=findU_parent(i);
+            int ult_j=findU_parent(j);
+            
+            if(ult_i == ult_j) return;
+            
+            if(size[ult_i]<size[ult_j]){
+                parent[ult_i]=ult_j;
+                size[ult_j]+=size[ult_i];
+            }
+            else{
+                parent[ult_j]=ult_i;
+                size[ult_i]+=size[ult_j];
+            }
+        }
+}; 
+class Solution {
+  public:
+    int maxRemove(vector<vector<int>>& stones, int N) {
+        int n=0,m=0;
+        for(int i=0;i<N;i++){
+            n=max(n,stones[i][0]);
+            m=max(m,stones[i][1]);
+        }
+        
+        DisjointSet ds(n+m+1);
+        
+        unordered_map<int,int>mapp;
+        
+        for(int i=0;i<N;i++){
+            int u=stones[i][0];
+            int v=stones[i][1]+n+1;
+            ds.unionBySize(u,v);
+            mapp[u]=1;
+            mapp[v]=1;
+        }
+        
+        int cnt=0;
+        for(auto it:mapp){
+            if(ds.findU_parent(it.first)==it.first){
+                cnt++;
+            }
+        }
+        return N-cnt;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < n; ++i) {
+            vector<int> temp;
+            for (int i = 0; i < 2; ++i) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution obj;
+
+        cout << obj.maxRemove(adj, n) << "\n";
+    }
+}
+// } Driver Code Ends
