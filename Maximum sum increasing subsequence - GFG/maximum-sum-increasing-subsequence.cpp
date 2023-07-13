cd1013adffe50c03a7410b02178c00e08ca920b5
Vishal Khurana <98726988+iamvishalkhurana@users.noreply.github.com>
2023-07-13 11:02:48 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -7,29 +7,25 @@ class Solution{
 		
     
 	public:
-	
-	int rec(int arr[],int i,int n,int prev,vector<vector<int>>&dp){
-	    if(i==n){
-	        return 0;
-	    }
-	    
-	    int take=0;
-	    
-	    if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-	    
-	    
-	    if(prev==-1 || arr[i]>arr[prev]){
-	        take=arr[i]+rec(arr,i+1,n,i,dp);
-	    }
-	    
-	    int nottake=rec(arr,i+1,n,prev,dp);
-	    
-	    return dp[i][prev+1]=max(take,nottake);
-	}
 	int maxSumIS(int arr[], int n)  
 	{  
-	    vector<vector<int>>dp(n,vector<int>(n+1,-1));
-	    return rec(arr,0,n,-1,dp);
+	    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+	    
+	    for(int i=n-1;i>=0;i--){
+	        
+	        for(int prev=i-1;prev >=-1 ;prev--){
+	            int take=0;
+	    
+        	    if(prev==-1 || arr[i]>arr[prev]){
+        	        take=arr[i]+dp[i+1][i+1];
+        	    }
+        	    
+        	    int nottake=dp[i+1][prev+1];
+        	    
+        	    dp[i][prev+1]=max(take,nottake);
+	        }
+	    }
+	    return dp[0][0];
 	}  
 };
 
