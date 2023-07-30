cf9ad5c7d50c4f6ca3284b1ea49d16251489ec7c
Vishal Khurana <98726988+iamvishalkhurana@users.noreply.github.com>
2023-07-30 14:53:57 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -8,27 +8,17 @@ using namespace std;
 class Solution{
     public:
     int mod=1e9+7;
-    long long countways(int n,int k,vector<int>&dp){
-        if(n==1) return k;
-        
-        if(n==2) return k*k % mod;
-        
-        if(dp[n]!=-1) return dp[n];
-        
-        long long ans=((countways(n-2,k,dp)*(k-1))%mod + (countways(n-1,k,dp)*(k-1))%mod)%mod;
-        
-        return dp[n]=ans;
-    }
     long long countWays(int n, int k){
-        vector<long long>dp(n+1,0);
-        dp[1]=k;
-        dp[2]=(k*k) % mod;
+        
+        long long dp1=k;
+        long long dp2=(k*k) % mod;
         
         for(int i=3;i<=n;i++){
-            long long ans= ( (dp[i-2]*(k-1))%mod + ( dp[i-1]*(k-1) )%mod) %mod;
-            dp[i]=ans;
+            long long curr= ( (dp2*(k-1))%mod + ( dp1*(k-1) )%mod) %mod;
+            dp1=dp2;
+            dp2=curr;
         }
-        return dp[n];
+        return n==1 ? dp1 : dp2;
     }
 };
 
